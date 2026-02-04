4b45ec61d34ff4cc26b79b3d06ba5f51d7e37cbb
tasniaarhamarham <tasniaarhamarham@gmail.com>
2026-02-04 16:42:37 +0600

    working with cart section

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,12 +1,24 @@
+import { getAllMeal } from '@/actions/meal.action';
+import { MealCard } from '@/components/common/card/mealcard';
 import FeatureSection from '@/components/modules/home/featureSection';
 import Hero from '@/components/modules/home/hero';
+import { mealTableType } from '@/types';
 import React from 'react';
 
-const page = () => {
+const page =async () => {
+   const { data } = await getAllMeal();
     return (
  <div>
     <Hero/>
-    <FeatureSection/>
+    <main className="min-h-screen bg-background container lg:mt-30 mt-10">
+      
+        <h2 className="text-3xl font-bold text-foreground mb-8">Popular Meals</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {data?.data?.slice(0,8)?.map((meal:mealTableType) => (
+            <MealCard key={meal.id} mealData={meal} />
+          ))}
+        </div>
+      </main>
  </div>
     );
 };
