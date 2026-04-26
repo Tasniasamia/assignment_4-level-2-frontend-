46ebdeaff34037425eb9f7cff8b9b5dc982ccd75
tasniaarhamarham@gmail.com <tasniaarhamarham@gmail.com>
2026-04-26 15:25:47 +0600

    asdfadsfdsf

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from 'next'
  
 const config: NextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: "images.unsplash.com",
-   
       },
     ],
   },
